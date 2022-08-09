74c86770d2add1f8e156dcd1bada7d4cf9d51c51
rodrigogollo <rodrigogollo.nh@gmail.com>
2022-08-08 21:40:39 -0300

    created the youtube upload automation, i think its working but i'm out of quotas for today

--- server/youtube/youtube.js
+++ server/youtube/youtube.js
@@ -1,75 +1,139 @@
 const fs = require('fs');
-const { google } = require('googleapis');
 const readline = require('readline');
+const assert = require('assert');
+const { google } = require('googleapis');
+const { inherits } = require('util');
 const OAuth2 = google.auth.OAuth2;
 
-// If modifying these scopes, delete your previously saved credentials
-// at ~/.credentials/upload_app_session.json
-const SCOPES = [
-  'https://www.googleapis.com/auth/youtube.upload',
-  'https://www.googleapis.com/auth/youtube.readonly'
-];
+const KEY_FILE_PATH = './client_key.json';
+const SCOPES = ['https://www.googleapis.com/auth/youtube.upload'];
+const TOKEN_PATH = './client_oauth_token.json';
 
-const TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';
-const TOKEN_PATH = TOKEN_DIR + 'upload_app_session.json';
+const videoFilePath = '../../out/video.mp4';
+const thumbFilePath = '../../out/thumb.png';
 
-const authorize = (credentials, cb) => {
+// video category IDs for Youtube:
+const categoryIds = {
+  Gaming: 20,
+  Entertainment: 24,
+}
+
+const init = (title, description, tags) => {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: KEY_FILE_PATH,
+    scopes: SCOPES
+  })
+  
+  const authClient = auth.getClient();
+  
+  const youtubeService = google.youtube({version: 'v3', auth: authClient });
+  // console.log(youtubeService)
+
+  assert(fs.existsSync(videoFilePath));
+  assert(fs.existsSync(thumbFilePath));
+
+  // fs.readFileSync('./client_secret.json', function processClientSecrets(err, content){
+  //   if(err){
+  //     console.log('error loading client secret file: ', err);
+  //   }
+    // Authorize a client with the loaded credentials, then call the Youtube API.
+    // authorize(JSON.parse(content), (auth) => uploadVideo(auth, title, description, tags));
+
+    uploadVideo(youtubeService, auth, title, description, tags)
+};
+
+function uploadVideo(service, auth, title, description, tags) {
+  
+  service.videos.insert({
+    auth: auth,
+    part: 'snippet,status',
+    requestBody: {
+      snippet: {
+        title,
+        description,
+        tags,
+        categoryId: "24",
+        defaultLanguage: 'en',
+        defaultAudioLanguage: 'en'
+      },
+      status: {
+        privacyStatus: 'private'
+      },
+    },
+    media: {
+      body: fs.createReadStream(videoFilePath)
+    },
+  }, function(err, response) {
+    if(err) {
+      console.log('The API returned an error: ', err);
+      return;
+    }
+    console.log(response.data);
+
+    console.log('Video uploaded. Uploading the thumbnail now.');
+    service.thumbnails.set({
+      auth: auth,
+      videoId: response.data.id,
+      media: {
+        body: fs.createReadStream(thumbFilePath)
+      },
+    }, function(err, response){
+      if(err){
+        console.log('The API returned an error: ', err);
+        return;
+      }
+      console.log(response.data);
+    })
+  });
+}
+
+function authorize(credentials, callback) {
   const clientSecret = credentials.installed.client_secret;
   const clientId = credentials.installed.client_id;
   const redirectUrl = credentials.installed.redirect_uris[0];
   const oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
 
   // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (error, token) => {
-      if (error) {
-          return getNewToken(oauth2Client, cb);
-      } else {
-          oauth2Client.credentials = JSON.parse(token);
-          return cb(null, oauth2Client);
-      }
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client, callback);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client);
+    }
   });
-};
+}
 
-const getNewToken = (oauth2Client, cb) => {
+function getNewToken(oauth2Client, callback) {
   const authUrl = oauth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: SCOPES
+    access_type: 'offline',
+    scope: SCOPES
   });
   console.log('Authorize this app by visiting this url: ', authUrl);
   const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
+    input: process.stdin,
+    output: process.stdout
   });
-  rl.question('Enter the code from that page here: ', code => {
-      rl.close();
-      oauth2Client.getToken(code, (error, token) => {
-          if (error) {
-              return cb(
-                  new Error(
-                      'Error while trying to retrieve access token',
-                      error
-                  )
-              );
-          }
-          oauth2Client.credentials = token;
-          storeToken(token);
-          return cb(null, oauth2Client);
-      });
-  });
-};
-
-const storeToken = token => {
-  try {
-      fs.mkdirSync(TOKEN_DIR);
-  } catch (error) {
-      if (error.code != 'EEXIST') {
-          throw error;
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
       }
-  }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token), error => {
-      if (error) throw error;
-      console.log('Token stored to ' + TOKEN_PATH);
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
   });
-};
+}
 
-module.exports = { authorize };
+function storeToken(token) {
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+    if (err) throw err;
+    console.log('Token stored to ' + TOKEN_PATH);
+  });
+}
+
+// uploadVideo('title test', 'description test', ['twitch', 'game'])
+init('title test', 'description test', ['twitch', 'game'])
